c6c03e5d90788556a2285e914c25e69a65f9f55e
jselvey2020 <jselvey2020@outlook.com>
2020-02-16 12:59:40 -0600

    new

---
+++ ConsoleApp1/Program2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Second Line Of Code");
+            Console.WriteLine("Third Line Of Code");
+            Console.WriteLine("Fourth Line Of Code");
+            Console.WriteLine("moi");
+
+        }
+    }
+}
+
+
