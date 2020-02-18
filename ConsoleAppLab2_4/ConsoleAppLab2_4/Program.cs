101eb841ac62899b55bc93666dc0a8b9724ca9ce
jselvey2020 <61116628+jselvey2020@users.noreply.github.com>
2020-02-18 10:38:35 -0600

    Add files via upload

---
+++ ConsoleAppLab2_4/ConsoleAppLab2_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleAppLab2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var myFirstNumber = 19;
+          var mySecondNumber = 244;
+            Console.WriteLine(myFirstNumber * mySecondNumber);
+            string myText = "Some Text";
+            Console.WriteLine(myText + mySecondNumber);
+            string moreText = "Goes Here";
+            Console.WriteLine(myText+moreText);
+            string color1 = "Yellow";
+            string color2 = "Red";
+            bool isYellow = true;
+            bool isGreen = false;
+            Console.WriteLine(color1 +isYellow + color2 + isGreen);
+            double numbers;
+            numbers = mySecondNumber / myFirstNumber;
+            Console.WriteLine(numbers);
+            numbers = mySecondNumber / (double)myFirstNumber;
+            Console.WriteLine(numbers);
+            Console.WriteLine("BreakPoint Here");
+
+        }
+    }
+}
