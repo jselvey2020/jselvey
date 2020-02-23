7521dc35960f938b331600b7e9af687b0aa847c4
jselvey2020 <61116628+jselvey2020@users.noreply.github.com>
2020-02-23 11:46:39 -0600

    Add files via upload

--- ConsoleAppLab3_4/ConsoleAppLab3_4/Program.cs
+++ ConsoleAppLab3_4/ConsoleAppLab3_4/Program.cs
@@ -6,6 +6,11 @@ public class Example
     public static void Main()
     {
         string pattern = @"[A-Z]";
+        string pattern1 = @"[d-e]";
+        string pattern2 = @"[d-e][*d]";
+        string pattern3 = @"[a][a-z][a]";
+        string pattern4 = @"[ ][p][a-z]*";
+        string pattern5 = @"[a-z]*[e][ ]";
         string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden
 base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies
 managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored
@@ -15,6 +20,26 @@ save her people and restore freedom to the galaxy….";
         RegexOptions options = RegexOptions.Multiline;
 
         foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+        foreach (Match m in Regex.Matches(input, pattern1, options))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+        foreach (Match m in Regex.Matches(input, pattern2, options))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+        foreach (Match m in Regex.Matches(input, pattern3, options))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+        foreach (Match m in Regex.Matches(input, pattern4, options))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+        foreach (Match m in Regex.Matches(input, pattern5, options))
         {
             Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
         }
