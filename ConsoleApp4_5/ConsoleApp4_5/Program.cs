4926b213631bbbf9d9cada6e1724b722db4223ed
jselvey2020 <61116628+jselvey2020@users.noreply.github.com>
2020-02-25 11:27:35 -0600

    Add files via upload

---
+++ ConsoleApp4_5/ConsoleApp4_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> highScores = new List<int>();
+            
+                highScores.Add(4400);
+                highScores.Add(8900);
+                highScores.Add(1200);
+                highScores.Add(3700);
+                highScores.Add(7200);
+
+                highScores.Sort();
+
+            
+                highScores.Reverse();
+            foreach (int score in highScores)
+            {
+
+                Console.WriteLine(score);
+            }
+
+            highScores.Add(2400);
+
+            highScores.Remove(4400);
+
+
+
+
+        }
+    }
+}
