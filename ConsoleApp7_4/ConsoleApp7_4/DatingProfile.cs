c3db1375c6922311b7638d3d297a3a9db495088b
jselvey2020 <61116628+jselvey2020@users.noreply.github.com>
2020-03-08 11:30:02 -0500

    Add files via upload

---
+++ ConsoleApp7_4/ConsoleApp7_4/DatingProfile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7_4
+{
+    class DatingProfile
+    {
+        public string firstName;
+        private string lastName;
+        public int age;
+        public string gender;
+        public string bio;
+        private List<Messages> myMessages;
+
+        public DatingProfile (string firstName, string lastName, int age, string gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.gender = gender;
+            this.bio = bio;
+            myMessages = new List<Messages>();
+        }
+
+        public void WriteBio(string text)
+        {
+            bio = text;
+        }
+
+        public void AddToInbox(Messages message)
+        {
+            myMessages.Add(message);
+                
+        }
+
+        public void SendMessage(string messageTitle, string messageData, DatingProfile sentTo)
+        {
+            Messages message = new Messages(this, messageTitle, messageData);
+            sentTo.AddToInbox(message);
+        }
+        public void ReadMessage()
+        {
+            foreach (Messages message in myMessages)
+            {
+                if (message.isRead == false)
+                {
+                    Console.WriteLine(message.messageTitle);
+                    Console.WriteLine(message.messageData);
+                    message.isRead = true;
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ ConsoleApp7_4/ConsoleApp7_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatingProfile rob = new DatingProfile("Rob", "Szybilski", 29, "Male");
+            rob.WriteBio("Loves D&D and Spicy Food");
+
+            DatingProfile penny = new DatingProfile("Penny", "Newman", 34, "Female");
+            penny.WriteBio("Looking for new friend with interesting hobbies.");
+
+            rob.SendMessage("Hey Penny", "Want to grab a cup of coffee?", penny);
+            penny.ReadMessage();
+        }
+    }
+}
